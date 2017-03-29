d27d785339a856f143916acc31481c019c5fcbae
Tdme <1092648985@qq.com>
2017-03-29 15:18:47 +0800

    Second

---
+++ app/src/main/java/com/tdme/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package com.tdme.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2017/3/28.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
